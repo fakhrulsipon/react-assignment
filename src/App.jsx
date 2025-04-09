874957b4e37e44d28a56539301c81481136ae929
sipon <fakhrulislamsipon@gmail.com>
2025-04-09 07:45:09 +0600

    my fourth commit file

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,15 @@ function App() {
         <p className='text-[20px]'>Discover and bid on extraordinary items</p>
       </div>
 
-      <div className='w-[92%] mx-auto'>
+      {/* <div className='w-[92%] mx-auto'>
          <div className='w-[70%]'>
          <div className='flex justify-between'>
-          <p>Item</p>
+          <div className='flex gap-20'>
+          <p>Item Image</p>
+          <p>Item Name</p>
+          </div>
           <div className='flex gap-16'>
-            <p>Current Bid</p>
+            <p>Current Bid Price</p>
             <p>Time Left</p>
             <p>Bid Now</p>
           </div>
@@ -42,7 +45,42 @@ function App() {
 
          </div>
 
-      </div>
+      </div> */}
+
+<div className="w-[92%] mx-auto">
+
+  <table className="w-[70%] border-separate border-spacing-4">
+    <thead>
+      <tr className="text-left">
+        <th className="text-[18px] font-medium">Item Image</th>
+        <th className="text-[18px] font-medium">Item Name</th>
+        <th className="text-[18px] font-medium">Current Bid Price</th>
+        <th className="text-[18px] font-medium">Time Left</th>
+        <th className="text-[18px] font-medium">Bid Now</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <img src="/path/to/image.jpg" alt="Item Image" className="w-[50px] h-[50px] object-cover" />
+        </td>
+        <td>First Edition 'The Great Gatsby' by F. Scott Fitzgerald</td>
+        <td>$150</td>
+        <td>12h 30m</td>
+        <td>
+          <button className="py-2 px-4 bg-blue-500 text-white rounded-xl">Bid Now</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div className='w-[30%]'>
+    <div>
+    <h1>Favorite Items</h1>
+    </div>
+
+  </div>
+</div>
      
     </>
   )
